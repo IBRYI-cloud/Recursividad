fc351bc8e81012ee15cf7ce34ac892759ba6e559
Jose Rivera <riveraalbinojose@gmail.com>
2021-09-15 13:06:52 -0400

    Suma vectores

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,3 +1,5 @@
+import java.io.PrintStream;
+
 public class Factorial {
     /**
      * 5! = 5 * 4 * 3 * 2 * 1
@@ -12,7 +14,6 @@ public class Factorial {
 
         return factorial;
     }
-
 
     public static int factorial(int numero){
         int factorial = 0;
@@ -36,5 +37,23 @@ public class Factorial {
                 mcd = mcd(a, b);
         }
         return mcd;
+    }
+    /*
+    suma de elementos de un vector
+    vector = {2, 4, 6}
+    suma = 2 + {4, 6}
+    SUMA = 4 + {6}
+    suma = 6 (caso base)
+     */
+    public static int sumaVector(int[] vector, int indice){
+        int suma;
+        if (indice == vector.length - 1){
+            //caso base
+            suma = vector[indice];
+
+        }else {
+                suma = vector [indice] + sumaVector(vector, indice + 1);
+        }
+        return suma;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String args[]){
         //System.out.println(Factorial.FactorialSinRecursividad( 12));
         //System.out.println(Factorial.factorial(5));
-        System.out.println(Factorial.mcd(412, 184));
+        //System.out.println(Factorial.mcd(412, 184));
+        int[] vector = {2, 4, 6};
+        System.out.println(Factorial.sumaVector(vector, 8));
     }
 }
